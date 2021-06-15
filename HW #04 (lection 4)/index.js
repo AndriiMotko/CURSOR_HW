0acf716579874de2ea04fdbf6dc230697b79901e
AndriiMotko <andrylviv@gmail.com>
2021-06-15 21:12:05 +0300

    HW #04 (lection 4) test 2

--- HW #04 (lection 4)/index.js
+++ HW #04 (lection 4)/index.js
@@ -5,7 +5,6 @@ const marks = [4, 5, 5, 3, 4, 5];
 const pairs = getPairs(students);
 const pairsStudentsTheme = getPairsStudentTheme(pairs, themes);
 
-
 // Задача №01 
 // Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом. У вас повинен вийти вкладений масив з парами студентів: [["Олександр", "Олена"], [..], [...]]; 
 
@@ -24,14 +23,16 @@ function getPairs(arr) {
 // Задача №02
 // Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати. Повинен вийти вкладений масив виду: [["Олександр і Олена", "Теорія автоматів"], [...], [...]]
 
-function getPairsStudentTheme(pairs, themes) {
-    let pairsStudentsTheme = [].concat(pairs);
+function getPairsStudentTheme(arr1, arr2) {
     
-    for(let i = 0; i < pairs.length; i++) {
-        pairsStudentsTheme[i].push(themes[i]);
+    const pairsStudentsAndTheme = [].concat(arr1);
+
+    for(let i = 0; i < pairsStudentsAndTheme.length; i++) {
+        pairsStudentsAndTheme[i].push(arr2[i]);
     }
-    return pairsStudentsTheme;
+    return pairsStudentsAndTheme;
 }
+
 
 console.log("_______________________________________________________");
 console.log(pairs);
